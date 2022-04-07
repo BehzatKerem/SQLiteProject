f808f6b882bf3c1396ba355eb071230770b8d1d3
BehzatKerem <62844191+BehzatKerem@users.noreply.github.com>
2022-04-07 16:56:40 +0300

    SQLite Project

---
+++ app/src/main/java/com/dursunbehzat/sqliteproject/MainActivity.java
@@ -0,0 +1,23 @@
+package com.dursunbehzat.sqliteproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        try{
+            SQLiteDatabase database = this.openOrCreateDatabase("Musicians",MODE_PRIVATE, null);
+            database.execSQL("CREATE TABLE IF NOT EXISTS musicians(name VARCHAR, age INT)");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
